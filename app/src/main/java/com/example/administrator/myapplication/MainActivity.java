8b989f8fb769201159c769fedb7ec3b0b84be881
nafiulakbar <nafiul.akbar@northsouth.edu>
2015-10-18 23:56:16 +0600

    ahshhsd

--- app/src/main/java/com/example/administrator/myapplication/MainActivity.java
+++ app/src/main/java/com/example/administrator/myapplication/MainActivity.java
@@ -1,9 +1,14 @@
 package com.example.administrator.myapplication;
 
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +16,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final SharedPreferences share=getSharedPreferences("Highest", MODE_PRIVATE);
+        final SharedPreferences.Editor change= share.edit();
+
+        final EditText in=(EditText)findViewById(R.id.number);
+        final TextView out=(TextView)findViewById(R.id.highnumber);
+        final Button   store=(Button)findViewById(R.id.store);
+
+        store.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+             SharedPreferences res=getSharedPreferences("Highest",MODE_PRIVATE);
+             int ref=res.getInt("highest",0);
+                String num=in.getText().toString();
+                int newnum=Integer.parseInt(num);
+
+                if(newnum>ref)
+                {ref=newnum;
+                    change.putInt("highest",newnum);
+                    change.commit();
+
+            }
+
+            out.setText(ref+"");
+        }});
     }
 
     @Override
